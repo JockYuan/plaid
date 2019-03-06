f6a9fc9fed9247c1bb10cecc8f08074b7bd98922
Manuel Vivo <mvivo@google.com>
2019-03-06 14:21:08 +0100

    Add UiModel to Dribbble Shot

---
+++ dribbble/src/test/java/io/plaidapp/dribbble/domain/CreateShotUiModelUseCaseTest.kt
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2019 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.plaidapp.dribbble.domain
+
+import android.content.res.ColorStateList
+import android.text.SpannableStringBuilder
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import io.plaidapp.core.util.HtmlParser
+import io.plaidapp.dribbble.testShot
+import io.plaidapp.dribbble.ui.shot.ShotDecorator
+import io.plaidapp.test.shared.provideFakeCoroutinesDispatcherProvider
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import java.util.Locale
+
+class CreateShotUiModelUseCaseTest {
+
+    private val mockHtmlParser: HtmlParser = mock()
+    private val mockShotDecorator: ShotDecorator = mock {
+        on { linkColors } doReturn ColorStateList(arrayOf(intArrayOf(1)), intArrayOf(0xff00ff))
+        on { highlightColor } doReturn 0xff00ff
+        on { locale } doReturn Locale.US
+    }
+
+    private lateinit var createShotUiModel: CreateShotUiModelUseCase
+
+    @Before
+    fun setup() {
+        createShotUiModel =
+            CreateShotUiModelUseCase(mockHtmlParser, mockShotDecorator, provideFakeCoroutinesDispatcherProvider())
+
+        givenParsedHtml(testShot.description)
+    }
+
+    @Test
+    fun createModel_copiesFieldsFromShot() = runBlocking {
+        val result = createShotUiModel(testShot)
+
+        assertEquals(testShot.id, result.id)
+        assertEquals(testShot.title, result.title)
+        assertEquals(testShot.htmlUrl, result.url)
+        assertEquals(testShot.likesCount, result.likesCount)
+        assertEquals(testShot.viewsCount, result.viewsCount)
+        assertEquals(testShot.createdAt, result.createdAt)
+        assertEquals(testShot.images.best(), result.imageUrl)
+        assertEquals(testShot.images.bestSize(), result.imageSize)
+        assertEquals(testShot.user.name.toLowerCase(), result.userName)
+        assertEquals(testShot.user.highQualityAvatarUrl, result.userAvatarUrl)
+    }
+
+    @Test
+    fun createModel_formatsDescription() = runBlocking {
+        val result = createShotUiModel(testShot)
+
+        assertTrue(result.formattedDescription.isNotEmpty())
+        assertFalse(result.shouldHideDescription)
+    }
+
+    @Test
+    fun createModelWithoutDescription_hidesDescription() = runBlocking {
+        val noDescriptionShot = testShot.copy(
+            description = ""
+        )
+        givenParsedHtml(noDescriptionShot.description)
+
+        val result = createShotUiModel(noDescriptionShot)
+
+        assertTrue(result.formattedDescription.isEmpty())
+        assertTrue(result.shouldHideDescription)
+    }
+
+    @Test
+    fun createModel_formatsViewCount() = runBlocking {
+        val result = createShotUiModel(testShot)
+        assertEquals("1,234", result.formattedViewsCount)
+    }
+
+    @Test
+    fun createModel_formatsLikesCount() = runBlocking {
+        val result = createShotUiModel(testShot)
+        assertEquals("5,678", result.formattedLikesCount)
+    }
+
+    private fun givenParsedHtml(parsedString: String) {
+        val mockSpannableStringBuilder: SpannableStringBuilder = mock {
+            on { length } doReturn parsedString.length
+        }
+        whenever(mockHtmlParser.parse(any(), any(), any())).thenReturn(mockSpannableStringBuilder)
+    }
+}
